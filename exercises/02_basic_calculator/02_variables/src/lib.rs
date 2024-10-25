6383a3c643491151bd55ed69601e86569ded27b5
Vaughan <56775959+vrd83@users.noreply.github.com>
2024-10-25 21:17:15 +1100

    updates

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -10,7 +10,7 @@ pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     //  Do you need to annotate the type of `distance`? Why or why not?
 
     let distance = end - start;
-    
+
     // Don't change the line below
     distance / time_elapsed
 }
